de3a59106af27312a66251b981e7627b480ea059
Silvio Huan Matheus Grein Cristo Ribeiro <silviohuan@gmail.com>
2025-07-17 16:38:30 -0400

    Pequnas melhorias no css do footer

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import './style.css'
 
 
 const app = document.querySelector<HTMLDivElement>('#app')!
-// const itens = document.querySelector<HTMLDivElement>('.itens')!
-// const sho = document.querySelector<HTMLImageElement>('.show-hiden-options')!
 const itens = document.querySelectorAll('.itens')!
 const shos = document.querySelectorAll('.show-hiden-options')!
 const urlCompleta = window.location.protocol +'//' + window.location.host
@@ -11,34 +9,24 @@ const menu = document.querySelector<HTMLImageElement>('.menu')!
 const ul = document.querySelector<HTMLDivElement>('ul')!
 const ifro = document.querySelector<HTMLDivElement>('.logo-ifro')!
 ul.classList.toggle('hide')
-// for(let i:number = 0; i<itens.length; i++){
-//     const botao = shos[i] as HTMLImageElement;
-//     let opcoes = itens[i]?.children.length || 0
-//     const filho = itens[i]?.children[opcoes-1] as HTMLDivElement;
-//     filho.style.display = filho.style.display === 'none' ? 'flex':'none'
-//     botao.src = botao.src === urlCompleta + '/public/circle-plus.svg' ?
-//     urlCompleta + '/public/circle-minus.svg':
-//     urlCompleta + '/public/circle-plus.svg';
-// }
 let filhos:HTMLDivElement[] = []
+let botoes:HTMLImageElement[] = []
 for(let i:number = 0; i<itens.length; i++){
     shos[i].addEventListener('click', () =>{
     const botao = shos[i] as HTMLImageElement;
     let opcoes = itens[i]?.children.length || 0
     const filho = itens[i]?.children[opcoes-1] as HTMLDivElement;
     filho.style.display = filho.style.display === 'none' ? 'flex':'none'
-    botao.src = botao.src === urlCompleta + '/public/circle-plus.svg' ?
-    urlCompleta + '/public/circle-minus.svg':
-    urlCompleta + '/public/circle-plus.svg';
+    botao.src = botao.src === urlCompleta + '/public/circle-minus.svg' ?
+    urlCompleta + '/public/circle-plus.svg':
+    urlCompleta + '/public/circle-minus.svg';
     itens[i].classList.toggle('ativo')
-    // console.log(botao.classList)
-    // console.log(botao.src)
-    // console.log(urlCompleta)
     filhos.push(filho)
+    botoes.push(botao)
+
     })
 }
 menu?.addEventListener('click', ()=>{
-    // ul.style.display = ul.style.display === 'none' ? 'flex':'none'
     menu.style.opacity = '0';
     setTimeout(() =>{
     menu.src = menu.src === urlCompleta + '/public/align-justify.svg' ? 
@@ -50,13 +38,18 @@ menu?.addEventListener('click', ()=>{
 })
 window.addEventListener('resize', () =>{
     if(window.innerWidth >= 768){
+        ifro.style.display = 'flex'
         for(const filho of filhos){
             filho.style.display = 'flex'
         }
         if(menu.src === urlCompleta + '/public/x.svg'){
             menu.click()
         }
-        ifro.style.display = 'flex'
+        if(botoes[0].src === urlCompleta + '/public/circle-plus.svg'){
+            for(const botao of botoes){
+                botao.src = urlCompleta + '/public/circle-minus.svg'
+            }
+        }
     } else {
         ifro.style.display = 'none'
         
